097c66abbe6e0ada9d4aa49286487b513b62cefb
Abhishek S R <abhishekreju007@gmail.com>
2023-01-27 12:38:42 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/navbar/Navbar";
+import RowPost from "./components/rowpost/RowPost";
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
+     <Navbar/>
+     <Banner/>
+     <RowPost/>
     </div>
   );
 }
